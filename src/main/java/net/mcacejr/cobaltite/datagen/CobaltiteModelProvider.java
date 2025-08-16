011c3a76f2bc7fb6d48cfd9b4d315290f1e7fb85
McAceJr <mcacejunior@gmail.com>
2025-08-16 19:55:34 -0400

    Added inventory model generation for kaleidoscopes

--- src/main/java/net/mcacejr/cobaltite/datagen/CobaltiteModelProvider.java
+++ src/main/java/net/mcacejr/cobaltite/datagen/CobaltiteModelProvider.java
@@ -42,6 +42,28 @@ public class CobaltiteModelProvider extends FabricModelProvider {
 
         itemModelGenerator.register(CobaltiteItems.OPAL, Models.GENERATED);
 
+        itemModelGenerator.register(CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.ART_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.BUMPY_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.BLOBS2_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.PENCIL_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.COLOR_CONVOLVE_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.DECONVERGE_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.FLIP_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.INVERT_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.NTSC_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.PHOSPHOR_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.SCAN_PINCUSHION_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.SOBEL_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.BITS_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.DESATURATE_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.GREEN_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.WOBBLE_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.BLOBS_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.CREEPER_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.SPIDER_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+        itemModelGenerator.register(CobaltiteItems.BLINDING_KALEIDOSCOPE, CobaltiteItems.NOTCH_KALEIDOSCOPE, Models.GENERATED);
+
     }
 
 }
